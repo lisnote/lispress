5161ddbd048c26d7d0b2faaa8a46467f4326449e
lisnote <didongxiaoli@gmail.com>
2022-04-28 05:22:43 +0800

    add lispress

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import lispress from 'lispress';
+
+lispress.config({
+  username: 'lisnote',
+  clientID: 'de3105a8fb15edf7ca19',
+  clientSecret: '715c4a9cfe522069f914ff3f839119e6c4ae73a8',
+});
 
 createApp(App).mount('#app');

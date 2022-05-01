195580f942eec482ae86d3dc13a1f2bc6d2e5102
lisnote <didongxiaoli@gmail.com>
2022-05-02 01:01:03 +0800

    add chapter jump feature

--- src/views/ChaptersIndex.vue
+++ src/views/ChaptersIndex.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import store from '../store';
-import { computed } from 'vue';
+import { computed, nextTick } from 'vue';
 let chapters = computed(() => {
   return store.article.querySelectorAll('h1,h2,h3,h4,h5,h6');
+});
+nextTick(() => {
+  let chaptersIndex = document.querySelector('#chapters-index');
+  chaptersIndex?.addEventListener('click', (e) => {
+    let id = e.target.dataset.id;
+    if (id?.indexOf('#') == 0) {
+      document.documentElement.scrollTop = document.querySelector(id).offsetTop;
+    }
+  });
 });
 </script>
 
@@ -11,10 +20,10 @@ let chapters = computed(() => {
     <div
       v-for="chapter in chapters"
       :key="chapter.id"
-      :href="`#${chapter.id}`"
+      :data-id="`#${chapter.id}`"
       :style="{ '--level': chapter.tagName.substring(1) * 10 + '' }"
     >
-      {{ chapter.innerHTML }}
+      {{ chapter.innerText }}
     </div>
   </div>
 </template>
--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.listen(80);
+
+let port = 8080;
+app.listen(port);
 app.use(express.static(path.resolve(__dirname, '../dist')));
+console.log(`http://localhost:${port}`);

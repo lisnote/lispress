06d469aac5b378f9a00d82e5889f1dad8fc9fa6a
lisnote <didongxiaoli@gmail.com>
2022-05-04 21:16:17 +0800

    pinia config fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 
 export const mainStore = defineStore('main', {
-  state() {
+  state: () => {
     return {
       article: document.createElement('div'),
     };

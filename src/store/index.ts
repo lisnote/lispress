3f1f59cae5046fa81abb5c19141b70616784a79f
lisnote <didongxiaoli@gmail.com>
2022-05-05 17:13:42 +0800

    replenish for last commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { createPinia } from 'pinia';
 import PressStore from './PressStore';
-import CommonStore from './CommonStore';
 
 const pinia = createPinia();
 
-export { pinia as default, PressStore, CommonStore };
+export { pinia as default, PressStore };
